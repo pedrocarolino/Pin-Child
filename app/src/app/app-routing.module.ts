d6eb6bbfe159d5d51704e73d4c0d86e1ad433a72
Pedro Carolino <pedrocarolino@live.com>
2019-05-26 01:17:08 -0300

    Finished page mainMenu

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/pages/home/home.module#HomePageModule' },
   { path: 'my-child', loadChildren: './menu/pages/mychild/mychild.module#MychildPageModule' },
   { path: 'add-child', loadChildren: './menu/pages/add-child/add-child.module#AddChildPageModule' },
+  { path: 'main-menu', loadChildren: './menu/pages/main-menu/main-menu.module#MainMenuPageModule' },
 ];
 
 @NgModule({
